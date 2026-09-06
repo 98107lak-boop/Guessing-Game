e27323b244f657eea4dbec17cc1be98f7eef36ff
98107lak@gmail.com <98107lak@gmail.com>
2026-09-07 03:28:18 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is Guessing Game
+import  random
+print("==================================================================================")
+print("==============================GUESSING GAME=======================================")
+print("==================================================================================")
+
+n=random.randint(1,100)
+a=-1
+guesses=1
+while (a!=n):
+    a=int(input("Guess the number:"))
+    if (a>n):
+        print("Lower number please")
+        guesses+=1
+    elif(a<n):
+        print("HIgher number please")
+        guesses+=1
+print(f"YOU have guessed the number {n} correctly in {guesses} attempts")        
